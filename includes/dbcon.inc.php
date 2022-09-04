0fce1f66d08f3e6c2fbbc641a5ace2811ad854eb
kejs84 <kai.erik.jesse@mnd.thm.de>
2022-09-04 23:20:21 +0200

    dockerized mariadb for testing only

--- includes/dbcon.inc.php
+++ includes/dbcon.inc.php
@@ -4,7 +4,7 @@ if(!isset($_SESSION))
     session_start(); 
 } 
 // Verbindung zur Datenbank
-$con = mysqli_connect('localhost', 'root', '', 'fitnesstracker');
+$con = mysqli_connect('localhost', 'root', '', 'swt_crp');
 // sollte die Verbindung nicht funktionieren, dann wird eine Fehlermeldung ausgegeben
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
