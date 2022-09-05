b3257dcedb56b72944e4c91dcc5314d15534ac79
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2022-09-05 21:26:47 +0200

    added centent overview (popup modal not functional)

---
+++ content-overview.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Übersicht</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">    
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#addContentModal">Add New Content</button>
+  <div class="row mt-5 mb-2">
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row mt-5 mb-2">
+
+<div class="col-sm-4"> 
+  <div class="panel panel-primary">
+    <div class="card">
+      <div class="row no-gutters">
+        <div class="col-auto text-center">
+          <h4 class="card-title">Title</h4>
+          <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col">
+          <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col-sm-4"> 
+  <div class="panel panel-primary">
+    <div class="card">
+      <div class="row no-gutters">
+        <div class="col-auto text-center">
+          <h4 class="card-title">Title</h4>
+          <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col">
+          <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="col-sm-4"> 
+  <div class="panel panel-primary">
+    <div class="card">
+      <div class="row no-gutters">
+        <div class="col-auto text-center">
+          <h4 class="card-title">Title</h4>
+          <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col">
+          <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+</div><br>
+
+<div class="container">    
+  <div class="row my-5">
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-4"> 
+      <div class="panel panel-primary">
+        <div class="card">
+          <div class="row no-gutters">
+            <div class="col-auto text-center">
+              <h4 class="card-title">Title</h4>
+              <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="col">
+              <div class="card-block px-2 mx-1 overflow-hidden " style="max-height: 110px; text-align: justify;">
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae necessitatibus recusandae perferendis eius harum. Ipsum ut magni corrupti, nesciunt labore repudiandae sapiente aspernatur!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div><br>
+
+    
+</body>
+</html>
