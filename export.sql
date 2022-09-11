f45b062aeb9b5c44d927dbb2e248f91b8824db51
Dennis Thomas Wach <dennis.thomas.wach@mnd.thm.de>
2022-09-11 17:13:11 +0200

    add changed export from adminer

--- export.sql
+++ export.sql
@@ -1,105 +1,58 @@
--- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: swt_projekt
--- ------------------------------------------------------
--- Server version	10.4.24-MariaDB
+-- Adminer 4.8.1 MySQL 5.5.5-10.9.2-MariaDB-1:10.9.2+maria~ubu2204 dump
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
---
--- Table structure for table `content`
---
+SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `content`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `content` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Titel` varchar(255) NOT NULL,
   `Beschreibung` varchar(510) DEFAULT NULL,
+  `Bild` mediumblob DEFAULT NULL,
   `Kategorie` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `FK_kategorie` (`Kategorie`),
   CONSTRAINT `FK_kategorie` FOREIGN KEY (`Kategorie`) REFERENCES `kategorie` (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `content`
---
-
-LOCK TABLES `content` WRITE;
-/*!40000 ALTER TABLE `content` DISABLE KEYS */;
-/*!40000 ALTER TABLE `content` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `folgeliste`
---
+INSERT INTO `content` (`ID`, `Titel`, `Beschreibung`, `Bild`, `Kategorie`) VALUES
+(1,	'John Wick',	NULL,	NULL,	2),
+(2,	'John Wick 2',	NULL,	NULL,	2),
+(3,	'John Wick 3',	NULL,	NULL,	2),
+(4,	'Hunter X Hunter',	NULL,	NULL,	3),
+(5,	'Breaking Bad',	NULL,	NULL,	3);
 
 DROP TABLE IF EXISTS `folgeliste`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `folgeliste` (
   `folger` int(11) NOT NULL,
   `gefolgt` int(11) NOT NULL,
   PRIMARY KEY (`folger`,`gefolgt`),
-  KEY `folger` (`folger`,`gefolgt`),
   KEY `gefolgt` (`gefolgt`),
-  CONSTRAINT `folgeliste_ibfk_1` FOREIGN KEY (`gefolgt`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `folgeliste_ibfk_2` FOREIGN KEY (`folger`) REFERENCES `user` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  KEY `folger` (`folger`),
+  CONSTRAINT `folgeliste_ibfk_3` FOREIGN KEY (`folger`) REFERENCES `user` (`ID`),
   CONSTRAINT `gefolgtfk` FOREIGN KEY (`gefolgt`) REFERENCES `user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `folgeliste`
---
-
-LOCK TABLES `folgeliste` WRITE;
-/*!40000 ALTER TABLE `folgeliste` DISABLE KEYS */;
-/*!40000 ALTER TABLE `folgeliste` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `kategorie`
---
 
 DROP TABLE IF EXISTS `kategorie`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kategorie` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Kategoriebezeichnung` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `kategorie`
---
-
-LOCK TABLES `kategorie` WRITE;
-/*!40000 ALTER TABLE `kategorie` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kategorie` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `review`
---
+INSERT INTO `kategorie` (`ID`, `Kategoriebezeichnung`) VALUES
+(1,	'Videospiel'),
+(2,	'Film'),
+(3,	'Serie'),
+(4,	'Buch'),
+(5,	'Musik');
 
 DROP TABLE IF EXISTS `review`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `review` (
   `User` int(11) NOT NULL,
   `Content` int(11) NOT NULL,
@@ -107,100 +60,51 @@ CREATE TABLE `review` (
   `Bewertung` decimal(3,1) NOT NULL,
   `Timestamp` datetime NOT NULL,
   PRIMARY KEY (`User`,`Content`),
-  -- UNIQUE KEY `User` (`User`),
-  -- UNIQUE KEY `Content` (`Content`),
-  CONSTRAINT `review_ibfk_1` FOREIGN KEY (`User`) REFERENCES `user` (`ID`)
+  KEY `Content` (`Content`),
+  CONSTRAINT `review_ibfk_2` FOREIGN KEY (`Content`) REFERENCES `content` (`ID`),
+  CONSTRAINT `review_ibfk_3` FOREIGN KEY (`User`) REFERENCES `user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `review`
---
-
-LOCK TABLES `review` WRITE;
-/*!40000 ALTER TABLE `review` DISABLE KEYS */;
-/*!40000 ALTER TABLE `review` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `rolle`
---
+INSERT INTO `review` (`User`, `Content`, `Inhalt`, `Bewertung`, `Timestamp`) VALUES
+(1000,	1,	'Tolle Sache',	5.0,	'2019-03-10 02:55:05'),
+(1000,	3,	'Schwach',	2.0,	'2019-03-10 02:55:05'),
+(1000,	5,	'Klasse',	9.0,	'2019-03-10 02:55:05');
 
 DROP TABLE IF EXISTS `rolle`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rolle` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Rollenbezeichnung` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
---
--- Dumping data for table `rolle`
---
 
-LOCK TABLES `rolle` WRITE;
-/*!40000 ALTER TABLE `rolle` DISABLE KEYS */;
-/*!40000 ALTER TABLE `rolle` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `status`
---
+INSERT INTO `rolle` (`ID`, `Rollenbezeichnung`) VALUES
+(1,	'Moderator'),
+(2,	'User');
 
 DROP TABLE IF EXISTS `status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `status` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Statusbezeichnung` varchar(255) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `status`
---
-
-LOCK TABLES `status` WRITE;
-/*!40000 ALTER TABLE `status` DISABLE KEYS */;
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
 
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Username` varchar(30) NOT NULL,
   `PassHash` varchar(255) NOT NULL,
+  `Bild` mediumblob DEFAULT NULL,
   `Rolle` int(11) NOT NULL,
   PRIMARY KEY (`ID`),
   KEY `rollefk` (`Rolle`),
   CONSTRAINT `rollefk` FOREIGN KEY (`Rolle`) REFERENCES `rolle` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `watchlist`
---
+INSERT INTO `user` (`ID`, `Username`, `PassHash`, `Bild`, `Rolle`) VALUES
+(1000,	'tester',	'$2y$10$qZQ3luz0Wm6AQu94Jet.QOedYkwzAkACm1lfWdYOg5RdbXMlvA4Dm',	NULL,	2);
 
 DROP TABLE IF EXISTS `watchlist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `watchlist` (
   `User` int(11) NOT NULL,
   `Content` int(11) NOT NULL,
@@ -214,30 +118,6 @@ CREATE TABLE `watchlist` (
   CONSTRAINT `statusfk` FOREIGN KEY (`Status`) REFERENCES `status` (`ID`),
   CONSTRAINT `usefk` FOREIGN KEY (`User`) REFERENCES `user` (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `watchlist`
---
 
-LOCK TABLES `watchlist` WRITE;
-/*!40000 ALTER TABLE `watchlist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `watchlist` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-09-05 13:09:36
-INSERT INTO `rolle` VALUES (1, 'Moderator'), (2, 'User');
-INSERT INTO `user` VALUES (1000, 'tester', '$2y$10$qZQ3luz0Wm6AQu94Jet.QOedYkwzAkACm1lfWdYOg5RdbXMlvA4Dm', 2);
-INSERT INTO `kategorie` VALUES (1,'Videospiel'),(2,'Film'),(3,'Serie'),(4,'Buch'),(5,'Musik');
-INSERT INTO `content` VALUES (5,'John Wick',NULL,2),(6,'John Wick 2',NULL,2),(7,'John Wick 3',NULL,2),(8,'Hunter X Hunter',NULL,3),(9,'Breaking Bad',NULL,3);
-SET time_zone = 'UTC';
-INSERT INTO `review` VALUES (1000, 5, 'Tolle Sache', 5, '2019-03-10 02:55:05'), (1000, 7, 'Schwach', 2, '2019-03-10 02:55:05'), (1000, 9, 'Klasse', 9, '2019-03-10 02:55:05');
+-- 2022-09-11 15:08:26
