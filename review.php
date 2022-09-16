41f2b807623cafb9e94bc89f1199edfbd4d637ea
Alina Kammer <alina.kammer@mnd.thm.de>
2022-09-16 14:13:24 +0200

    added review

---
+++ review.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Review Content-Review-Plattform</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container-lg p-4">
+        <h1 class="text-center">Review</h1>
+        <div class="row">
+            <div class="col-sm-6 d-flex align-items-center mb-3">
+                <img src="" alt="" class="img"> 
+                <span class="m-3">
+                    <p class="mb-1">Von:</p>
+                    <p>Username</p>
+                </span>
+            </div>
+            <div class="col-sm-6 d-flex align-items-center justify-content-sm-end mb-3">
+                <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg" alt="" class="img">
+                <span class="m-3 order-sm-first">
+                    <p class="mb-1 d-flex justify-content-sm-end">Von:</p>
+                    <p class="d-flex justify-content-sm-end">Username</p>
+                </span>
+            </div>
+        </div>
+        <div class="border rounded">
+            <h4 class="text-center m-2">Rating 9/10</h4>
+            <p class="mx-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nesciunt repellat fuga, enim nobis debitis facere ad molestias nam ab nemo atque quaerat officia cumque provident consequuntur eaque sed velit ut rerum assumenda? Ipsa suscipit quam consequatur laboriosam aut nihil, illum a officiis dolores dolor deserunt quisquam quas sit rerum eligendi quae nisi, ea eveniet magni ab deleniti porro. Culpa minus dolorum placeat et ab. Ullam eligendi omnis ab inventore saepe nisi explicabo alias facilis fugit expedita, est iure culpa non dolorem sed delectus impedit. Temporibus veniam molestiae in soluta! Nostrum exercitationem recusandae dolorem, dolorum corporis a amet hic laudantium?</p>
+        </div>
+    </div>   
+</body>
+</html>
