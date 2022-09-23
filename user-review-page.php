a86a882b74537e44a6eb063ec6e5dc5056325211
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2022-09-23 09:51:48 +0200

    added user-review-page

---
+++ user-review-page.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alle Reviews</title>
+    <link href="css/content-page.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+    <?php
+  include './navbar.php';
+  ?>
+</head>
+
+<body>
+
+    <div class="text-center p-sm-4 pt-3">
+        <h4>Username</h4>
+        <div class="container">
+            <h4 class="text-center py-3">Alle Reviews:</h4>
+            <div class="row row-cols-1 row-cols-md-2 g-2 g-lg-3">
+
+            <!-- Single Review from user -->
+                <div class="col mb-3">
+                    <div class="panel panel-primary">
+                        <div class="card">
+                            <div class="row">
+                                <div class="d-flex px-4 justify-content-between">
+                                    <div class="d-flex ">
+                                        <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg"
+                                            alt="Logo" width="24" height="20" class="rounded-4">
+                                        <h6 class="text-start card-title">Content-xyz-abc</h6>
+                                    </div>
+                                    <h6 class="card-title">Rating: 7.2</h6>
+                                </div>
+
+                                <div class="col">
+                                    <div class="card-block px-2 mx-1" style="text-align: justify;">
+                                        <p class="card-text">Lorem ipsum dolor sit amet consectetur,
+                                            adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae
+                                            necessitatibus recusandae perferendis eius harum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <!-- End of single Review from user -->
+
+                <div class="col mb-3">
+                    <div class="panel panel-primary">
+                        <div class="card">
+                            <div class="row">
+                                <div class="d-flex px-4 justify-content-between">
+                                    <div class="d-flex ">
+                                        <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg"
+                                            alt="Logo" width="24" height="20" class="rounded-4">
+                                        <h6 class="text-start card-title">Content-xyz-abc</h6>
+                                    </div>
+                                    <h6 class="card-title">Rating: 7.2</h6>
+                                </div>
+
+                                <div class="col">
+                                    <div class="card-block px-2 mx-1" style="text-align: justify;">
+                                        <p class="card-text">Lorem ipsum dolor sit amet consectetur,
+                                            adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae
+                                            necessitatibus recusandae perferendis eius harum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col mb-3">
+                    <div class="panel panel-primary">
+                        <div class="card">
+                            <div class="row">
+                                <div class="d-flex px-4 justify-content-between">
+                                    <div class="d-flex ">
+                                        <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg"
+                                            alt="Logo" width="24" height="20" class="rounded-4">
+                                        <h6 class="text-start card-title">Content-xyz-abc</h6>
+                                    </div>
+                                    <h6 class="card-title">Rating: 7.2</h6>
+                                </div>
+
+                                <div class="col">
+                                    <div class="card-block px-2 mx-1" style="text-align: justify;">
+                                        <p class="card-text">Lorem ipsum dolor sit amet consectetur,
+                                            adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae
+                                            necessitatibus recusandae perferendis eius harum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col mb-3">
+                    <div class="panel panel-primary">
+                        <div class="card">
+                            <div class="row">
+                                <div class="d-flex px-4 justify-content-between">
+                                    <div class="d-flex ">
+                                        <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-600w-1037719192.jpg"
+                                            alt="Logo" width="24" height="20" class="rounded-4">
+                                        <h6 class="text-start card-title">Content-xyz-abc</h6>
+                                    </div>
+                                    <h6 class="card-title">Rating: 7.2</h6>
+                                </div>
+
+                                <div class="col">
+                                    <div class="card-block px-2 mx-1" style="text-align: justify;">
+                                        <p class="card-text">Lorem ipsum dolor sit amet consectetur,
+                                            adipisicing elit. Numquam provident sunt ab. Laudantium fuga, odit quae
+                                            necessitatibus recusandae perferendis eius harum.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+</body>
