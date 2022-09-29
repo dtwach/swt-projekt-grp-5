024d22664ecf7460bbc419d300b30eb6df92a952
Dennis Thomas Wach <dennis.thomas.wach@mnd.thm.de>
2022-09-29 21:05:35 +0200

    add local bootstrap

--- register.php
+++ register.php
@@ -11,10 +11,8 @@ if (!isset($_SESSION)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register Content-Review-Plattform</title>
     <link href="css/style.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.bundle.min.js">
     </script>
 </head>
 
@@ -35,8 +33,7 @@ if (!isset($_SESSION)) {
                 <input class="form-control" type="password" name="password" required placeholder="Passwort">
             </div>
             <div class="mb-3">
-                <input class="form-control" type="password" name="password_again" required
-                    placeholder="Passwort wiederholen">
+                <input class="form-control" type="password" name="password_again" required placeholder="Passwort wiederholen">
             </div>
             <input class="btn btn-primary w-50" name="register_submit" type="submit" value="Registrieren">
         </form>
